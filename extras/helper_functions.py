3484520a3c9d4275bb699ba98daf70344b6882dc
ingjorgeirm <ingjorgeirm@gmail.com>
2024-02-07 13:52:26 -0500

    Update helper_functions.py view_random_image(target_dir, target_class):

--- extras/helper_functions.py
+++ extras/helper_functions.py
@@ -286,3 +286,22 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+def view_random_image(target_dir, target_class):
+  #setup the target directory (we'll view image from here)
+  target_folder = target_dir + target_class
+
+  #get a random image path
+  random_image = random.sample(os.listdir(target_folder), 1)
+  print(random_image)
+
+  # Read in the image and plot it using matplotlib
+  img = mpimg.imread(target_folder + "/"+ random_image[0])
+
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off");
+  print(f"Image shape{img.shape}") #show the shape of the image
+
+  return img
+
